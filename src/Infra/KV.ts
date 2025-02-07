25ffa4b71965f6620691fe8b880dd2a872751497
Masato Yoshioka <yoshioka0627@gmail.com>
2025-02-07 21:58:11 +0900

    refactor: mv Infra Dir

---
+++ src/Infra/KV.ts
@@ -0,0 +1,48 @@
+export interface StorageEntity {
+  id: string;
+  [key: string]: unknown;
+}
+
+export interface StorageInterface<T extends StorageEntity> {
+  save(data: T): Promise<T>;
+  update(id: string, data: Partial<T>): Promise<void>;
+  findById(id: string): Promise<T | null>;
+}
+
+export class KVStorage<T extends StorageEntity> implements StorageInterface<T> {
+  private kv: Deno.Kv;
+  private prefix: string;
+
+  constructor(kv: Deno.Kv, prefix: string) {
+    this.kv = kv;
+    this.prefix = prefix;
+  }
+
+  private createKeySelector(id: string): string[] {
+    return [this.prefix, id];
+  }
+
+  async save(data: T) {
+    const keySelector = this.createKeySelector(data.id);
+    await this.kv.set(keySelector, data);
+    return data;
+  }
+  async update(id: string, data: Partial<T>): Promise<void> {
+    const keySelector = this.createKeySelector(id);
+    await this.kv.set(keySelector, data);
+    return;
+  }
+
+  async findById(id: string): Promise<T | null> {
+    const keySelector = this.createKeySelector(id);
+    const result = await this.kv.get<T>(keySelector);
+    return result.value;
+  }
+
+  static async create<T extends StorageEntity>(
+    prefix: string,
+  ): Promise<KVStorage<T>> {
+    const kv = await Deno.openKv();
+    return new KVStorage(kv, prefix);
+  }
+}
--- src/Repository/User.ts
+++ src/Repository/User.ts
@@ -1,6 +1,7 @@
 /// <reference lib="deno.unstable" />
 
 import { encode as base64Encode } from "https://deno.land/std@0.200.0/encoding/base64.ts";
+import { KVStorage, StorageEntity, StorageInterface } from "../Infra/KV.ts";
 
 type Profile = {
   username: string;
@@ -54,57 +55,6 @@ interface User extends StorageEntity {
   id: string;
   profile: Profile;
   authentication: Authentication;
-}
-
-interface StorageEntity {
-  id: string;
-  [key: string]: unknown;
-}
-
-interface StorageInterface<T extends StorageEntity> {
-  save(data: T): Promise<T>;
-  update(id: string, data: Partial<T>): Promise<void>;
-  findById(id: string): Promise<T | null>;
-  //delete(id: string): Promise<void>;
-}
-
-/** Infrasturacture **/
-class KVStorage<T extends StorageEntity> implements StorageInterface<T> {
-  private kv: Deno.Kv;
-  private prefix: string;
-
-  constructor(kv: Deno.Kv, prefix: string) {
-    this.kv = kv;
-    this.prefix = prefix;
-  }
-
-  private createKeySelector(id: string): string[] {
-    return [this.prefix, id];
-  }
-
-  async save(data: T) {
-    const keySelector = this.createKeySelector(data.id);
-    await this.kv.set(keySelector, data);
-    return data;
-  }
-  async update(id: string, data: Partial<T>): Promise<void> {
-    const keySelector = this.createKeySelector(id);
-    await this.kv.set(keySelector, data);
-    return;
-  }
-
-  async findById(id: string): Promise<T | null> {
-    const keySelector = this.createKeySelector(id);
-    const result = await this.kv.get<T>(keySelector);
-    return result.value;
-  }
-
-  static async create<T extends StorageEntity>(
-    prefix: string,
-  ): Promise<KVStorage<T>> {
-    const kv = await Deno.openKv();
-    return new KVStorage(kv, prefix);
-  }
 }
 
 interface UserRepositoryInterface {
