fa9d8c9fed329db7b070ac31e7d7f44592edf065
Masato Yoshioka <yoshioka0627@gmail.com>
2025-02-13 08:09:16 +0900

    fix: logout session domain

--- routes/logout.tsx
+++ routes/logout.tsx
@@ -1,13 +1,16 @@
-import {deleteCookie} from "@std/http/cookie"
+import { Handlers } from "$fresh/server.ts";
+import { deleteCookie } from "@std/http/cookie";
+
 export const handler: Handlers = {
-  async POST(req: Request, _ctx: any) {
+  POST(req, _ctx) {
     const headers = new Headers();
-    deleteCookie(headers, 'sess');
+    const url = new URL(req.url);
+    deleteCookie(headers, "sess", { domain: url.hostname});
     headers.set("Location", "/");
 
     return new Response(null, {
       status: 302,
-      headers
+      headers,
     });
   },
 };
